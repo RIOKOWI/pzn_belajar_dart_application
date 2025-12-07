6af4e7e8c1cc4198ade4ef3d69529b5e30d812d6
RIOKOWI <achyarrio2004@gmail.com>
2025-12-07 16:52:35 +0700

    dart application

---
+++ test/pzn_belajar_dart_application_test.dart
@@ -0,0 +1,8 @@
+import 'package:pzn_belajar_dart_application/pzn_belajar_dart_application.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
